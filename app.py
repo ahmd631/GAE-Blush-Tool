5f82f0b7ab4ba53e61e008e21edbdfd05b05af12
ahmd631 <hussain.ahmd631@gmail.com>
2026-06-01 15:13:42 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+import streamlit as st
 
+st.title("GAE Blush Annotation Tool")
+
+uploaded_file = st.file_uploader(
+    "Upload angiogram",
+    type=["png", "jpg", "jpeg"]
+)
+
+if uploaded_file:
+    st.success("Image uploaded!")
